e7135ec51e9e2b0bbb7c14d2b74698e5175b5661
gilad1987 <gilad1987@gmail.com>
2015-05-01 10:21:23 -0700

    init

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ var SCRIPTS_SRC = [
     'app/components/angular/angular.js',
     'app/components/angular-ui-router/release/angular-ui-router.js',
     'app/components/angular-bootstrap/ui-bootstrap-tpls.js',
-    'app/components/angular-recursion/angular-recursion.js',
+    //'app/components/angular-bootstrap/angular-recursion.js',
     //'app/components/angular-foundation/mm-foundation-tpls.js',
 
     'app/app/src/js/**/*.services.module.js',
